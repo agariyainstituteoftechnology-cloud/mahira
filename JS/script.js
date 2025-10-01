bb265947db5fa78fdf58afdc70bfd11b65fdf3d9
mahira <agariyainstituteoftechnology@gmail.com>
2025-10-01 13:17:21 +0500

    chnages in js fie

--- JS/script.js
+++ JS/script.js
@@ -33,3 +33,16 @@ if(isNaN(amount) || amount % 500 !== 0){
     }
 
 }
+
+// User se number lena
+var number = +prompt("Enter a number:");
+
+// Agar number valid hai to uska table print karega
+if (!isNaN(number)) {
+    console.log(number + " ka table:");
+    for (let i = 1; i <= 10; i++) {
+        console.log(number + " x " + i + " = " + (number * i));
+    }
+} else {
+    console.log("Please enter a correct number!");
+}
