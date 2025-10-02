fff0d4feaa8ad9df22d128a332888f4319834aec
mahira <agariyainstituteoftechnology@gmail.com>
2025-10-02 15:57:20 +0500

    added js file and figma css changes

--- JS/script.js
+++ JS/script.js
@@ -33,16 +33,8 @@ if(isNaN(amount) || amount % 500 !== 0){
     }
 
 }
+var user = prompt("do you want to any another transaction? yes or no")
+do{
+    console.log("yes or no")
 
-// User se number lena
-var number = +prompt("Enter a number:");
-
-// Agar number valid hai to uska table print karega
-if (!isNaN(number)) {
-    console.log(number + " ka table:");
-    for (let i = 1; i <= 10; i++) {
-        console.log(number + " x " + i + " = " + (number * i));
-    }
-} else {
-    console.log("Please enter a correct number!");
-}
+}while(user == "yes")
